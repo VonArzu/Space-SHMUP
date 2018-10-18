7b086a1799604a97b3a6f2d16957224ac80b970e
Von Arzu <vonarzu@gmail.com>
2018-10-18 19:11:29 -0400

    Spaceship and Enemies
    Created the main spaceship and sheild as well as created the 5 enemies starte=ing at enemy0

---
+++ Assets/_Scripts/Hero.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour {
+    static public Hero S;
+
+    [Header("Set in Inspector")]
+
+    public float speed = 30;
+    public float rollMult = 45;
+    public float pitchMult = 30;
+
+    [Header("Set Dynamically")]
+    public float sheildLevel = 1;
+
+
+
+    void Awake () {
+		if(S == null)
+        {
+            S = this;
+        }
+        else
+        {
+            Debug.LogError("Hero,Awake()- Attempt to assign second Hero.S!");
+        }
+	}
+	
+	void Update () {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+    }
+}
