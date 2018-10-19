dccff69c99b04a108627ccf50188313c5855dd51
Von Arzu <vonarzu@gmail.com>
2018-10-18 21:38:25 -0400

    Hero, Enemy, and BoundCheck
    editing and debugging c# script

--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -3,14 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
+    [Header("Set in Inspector: Enemy")]
+    public float speed = 10f;
+    public float fireRate = 0.3f;
+    public float health = 10;
+    public int   score = 100;
 
-	// Use this for initialization
-	void Start () {
-		
+    public Vector3 pos
+    {
+        get
+        {
+            return (this.transform.position);
+        }
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+    private BoundsCheck bndCheck;
+    void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+
+    void Update () {
+        Move();
+    if(bndCheck !=null && bndCheck.offDown)
+        {
+            Destroy(gameObject);
+        }
+
+    {
+        if(pos.y < bndCheck.camHeight - bndCheck.radius)
+            {
+                Destroy(gameObject);
+            }
+    }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public virtual void Move()
+    {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
 }
