45e61ef4e163ecb2a653764e0e1d1fbe07fedcbf
Von Arzu <vonarzu@gmail.com>
2018-11-01 18:55:24 -0400

    Script Add and TouchUps
    Deleted unnessacary items

--- Assets/_Scripts/Utils.cs
+++ Assets/_Scripts/Utils.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Utils : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class Utils : MonoBehaviour
+{
+    static public Material[] GetAllMaterials(GameObject go)
+    {
+        Renderer[] rends = go.GetComponentsInChildren<Renderer>();
+        List<Material> mats = new List<Material>();
+        foreach (Renderer rend in rends)
+        {
+            mats.Add(rend.material);
+        }
+        return (mats.ToArray());
+    }
 }
